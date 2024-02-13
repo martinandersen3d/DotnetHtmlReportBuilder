1704b2c31b0c267cc0f4215986a0a9445f42c980
Martin andersen <martinandersen3d@gmail.com>
2024-02-14 00:01:25 +0100

    minor improvements

--- DotnetHtmlReportBuilder/Program.cs
+++ DotnetHtmlReportBuilder/Program.cs
@@ -10,7 +10,7 @@ class Program
     {
         // Creating report data
 
-        // TABLE 1
+        // TABLE 1 -------------------------------------------------------
         var table1 = new ReportTable();
         table1.Title = "Title 1";
         table1.Description = "Description for table 1";
@@ -22,25 +22,21 @@ class Program
         table1.AddTableRecord(true, false, true); // booleans are converted to string
 
         // TABLE 1 - BUTTONS (Optional)
+        table1.AddButton("Link Button 1", "https://example.com/button1");
+        table1.AddButton("Link Button 2", "https://example.com/button2");
 
-        table1.Buttons = new IReportButton[]
-        {
-            new ReportButton { ButtonText = "Button 1", ButtonUrl = "https://example.com/button1" },
-            new ReportButton { ButtonText = "Button 2", ButtonUrl = "https://example.com/button2" }
-        };
-
-        // TABLE 2
+        // TABLE 2 -------------------------------------------------------
 
         var table2 = new ReportTable();
         table2.Title = "Title 2";
         table2.Description = "Description for table 2, Description for table 2, Description for table 2, Description for table 2, Description for table 2, Description for table 2, Description for table 2, Description for table 2, Description for table 2, Description for table 2";
-        table2.AddTableHeader("Header A", "Header B", "Header C");
+        table2.AddTableHeader("Header A", "Header B", "Header C", "Header D");
 
-        table2.AddTableRecord("Data A1", "Data B1", "Data C1");
-        table2.AddTableRecord("Data A2", "Data B2 Data B2 Data B2 Data B2 Data B2 Data B2 Data B2 Data B2 Data B2 ", "Data C2");
-        table2.AddTableRecord("Data A3", null, "Data C3"); // null value is treated as empty string
+        table2.AddTableRecord("Data A1", "Data B1", "Data C1", "Data D1");
+        table2.AddTableRecord("Data A2", "Data B2 Data B2 Data B2 Data B2 Data B2 Data B2 Data B2 Data B2 Data B2 ", "Data C2", "Data D2");
+        table2.AddTableRecord("Data A3", null, "Data C3", "Data D3"); // null value is treated as empty string
 
-        // Creating Report Page
+        // Creating Report Page ------------------------------------------
 
         var reportPage = new ReportPage
         {
