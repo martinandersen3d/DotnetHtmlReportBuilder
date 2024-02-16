4a66ea75c505569ac100adbd58aa82927cbb37e9
Martin andersen <martinandersen3d@gmail.com>
2024-02-16 09:53:24 +0100

    hover effect

--- DotnetHtmlReportBuilder/Helpers/HtmlReportHelper.cs
+++ DotnetHtmlReportBuilder/Helpers/HtmlReportHelper.cs
@@ -54,6 +54,8 @@ public class HtmlReportGenerator
         sb.AppendLine("td, th, a {font-size:16px !important;}");
         sb.AppendLine("p, span {font-size:16px !important;}");
         // sb.AppendLine(".shadow{box-shadow: 0 0 40px 0 rgba(0,0,0,.15);-moz-box-shadow: 0 0 40px 0 rgba(0,0,0,.15);-webkit-box-shadow: 0 0 40px 0 rgba(0,0,0,.15);-o-box-shadow: 0 0 40px 0 rgba(0,0,0,.15);-ms-box-shadow: 0 0 40px 0 rgba(0,0,0,.15);}");
+        sb.AppendLine(".data-table tr:hover>td {background-color:#dbf4ff;}");
+        sb.AppendLine(".data-table tr:hover { outline: 1px solid  #007bff;}");
         sb.AppendLine("</style>");
         sb.AppendLine("</head>");
         sb.AppendLine("<body><div>");
@@ -64,7 +66,7 @@ public class HtmlReportGenerator
 
             if (table.Description != "") sb.AppendLine("<p style=\"font-size:11pt;\">" + table.Description + "</p>");
 
-            sb.AppendLine("<table cellspacing=\"0\" cellpadding=\"0\" border=\"0\"  style=\"border-collapse: collapse; overflow: hidden; border: 1px solid #2f3030;\" >");
+            sb.AppendLine("<table class=\"data-table\" cellspacing=\"0\" cellpadding=\"0\" border=\"0\"  style=\"border-collapse: collapse; overflow: hidden; border: 1px solid #2f3030;\" >");
 
             // TABLE HEADERS
             if (table.TableHeaders != null && table.TableHeaders.Length > 0)
